9dc41c06159799cf6d2850ca629f07dcb40feae6
Moses Okemwa <okemwamoses@gmail.com>
2015-10-02 18:20:56 +0300

    added a view to display all bookmarks

--- marcador/views.py
+++ marcador/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_obbject_or_404, redirect, render
 
-# Create your views here.
+from .models import Bookmark
+
+#creating path from urls in marcador
+
+def bookmark_list(request):
+	bookmarks = Bookmark.public.all()
+	context = {'bookmarks': bookmarks}
+	return render(request, 'marcador/bookmark_list.html', context)
