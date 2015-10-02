f88cfa233c3745f340c01ea16d8ee39eb7006c06
Moses Okemwa <okemwamoses@gmail.com>
2015-10-02 18:43:04 +0300

    added view of all users

--- marcador/views.py
+++ marcador/views.py
@@ -1,10 +1,20 @@
+from django.contrib.auth.models import User
 from django.shortcuts import get_obbject_or_404, redirect, render
 
 from .models import Bookmark
 
 #creating path from urls in marcador
-
 def bookmark_list(request):
 	bookmarks = Bookmark.public.all()
 	context = {'bookmarks': bookmarks}
 	return render(request, 'marcador/bookmark_list.html', context)
+
+#view of all public bookmarks from a particular user
+def bookmark_user(request, username):
+	user = get_object_or_404(User, username=username)
+	if request.user == user:
+		bookmarks = user.bookmarks.all()
+	else:
+		bookmarks = Bookmark.public.filter(owner__username=username)
+	context = {'bookmarks': bookmarks, 'owner': user}
+	return render(request, 'marcador/bookmark_user.html', context)
