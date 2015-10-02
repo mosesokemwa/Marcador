c00f1909f1ca5d306dd9058a7c06d194eb4b8883
Moses Okemwa <okemwamoses@gmail.com>
2015-10-02 17:50:30 +0300

    working properly

--- marcador/admin.py
+++ marcador/admin.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
-
-from .models import Bookmark, Tag #makes the classes in models
-
+from .models import Bookmark, Tag
 
 
 class BookmarkAdmin(admin.ModelAdmin):
-    list_display = ('url', 'title', 'owner', 'is_public', 'is_updated')
-    list_editable =('is_public',)
-    list_filter = ('is_public', 'owner_username')
+    list_display = ('url', 'title', 'owner', 'is_public', 'date_updated')
+    list_editable = ('is_public',)
+    list_filter = ('is_public', 'owner__username')
     search_fields = ['url', 'title', 'description']
     readonly_fields = ('date_created', 'date_updated')
 
